6b012e0b92f73dddf2f89fb3b5f7834940e6ab90
Jaroslav Yushchenko <yaroslav.yushchenko1@gmail.com>
2022-02-20 22:43:14 +0200

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:  "https://api.finance.java.yushchenko.site"//"http://localhost:8080"
+  apiUrl: "http://localhost:8080" //"https://api.finance.java.yushchenko.site"
 };
